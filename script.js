75179a7a514106a5264dea153ead93e63e7a81f8
vo41 <161795582+vo41@users.noreply.github.com>
2024-08-22 17:40:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,15 @@
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
+// Set canvas for retina displays
+const scaleFactor = window.devicePixelRatio || 1;
+canvas.width = 750 * scaleFactor;
+canvas.height = 500 * scaleFactor;
+ctx.scale(scaleFactor, scaleFactor);
+
 // Canvas settings
-const canvasWidth = 750;
-const canvasHeight = 500;
+const canvasWidth = canvas.width;
+const canvasHeight = canvas.height;
 
 // Ship settings
 const shipImage = new Image();
@@ -19,36 +25,157 @@ const shipSpeed = 5;
 const moveSound = new Audio('sounds/move.mp3');
 let soundPlaying = false;
 
-// Planet settings
-const planetTypes = [
-    { name: 'planet_type0', color: 'red', size: 30 },
-    { name: 'planet_type1', color: 'blue', size: 40, moons: [{ size: 5, orbitRadius: 20 }] },
-    { name: 'planet_type2', color: 'green', size: 50, moons: [{ size: 5, orbitRadius: 20 }, { size: 10, orbitRadius: 30 }] },
-    { name: 'planet_type3', color: 'purple', size: 60, moons: [{ size: 5, orbitRadius: 20 }, { size: 10, orbitRadius: 30 }, { size: 15, orbitRadius: 40 }] }
+// Planet Types
+function createPlanet_type0() {
+    return {
+        name: 'planet_type0',
+        color: 'red',
+        size: 30,
+        speed: 1,
+        x: 0,
+        y: 0,
+        moons: [],
+        updatePosition: function() {
+            this.x -= this.speed;
+        },
+        draw: function(ctx) {
+            ctx.fillStyle = this.color;
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.size / 2, 0, Math.PI * 2);
+            ctx.fill();
+        }
+    };
+}
+
+function createPlanet_type1() {
+    return {
+        name: 'planet_type1',
+        color: 'blue',
+        size: 40,
+        speed: 1,
+        x: 0,
+        y: 0,
+        moons: [{ size: 5, orbitRadius: 20, angle: 0, orbitSpeed: 0.02 }],
+        updatePosition: function() {
+            this.x -= this.speed;
+            this.moons.forEach(moon => {
+                moon.angle += moon.orbitSpeed;
+                moon.x = this.x + Math.cos(moon.angle) * moon.orbitRadius;
+                moon.y = this.y + Math.sin(moon.angle) * moon.orbitRadius;
+            });
+        },
+        draw: function(ctx) {
+            ctx.fillStyle = this.color;
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.size / 2, 0, Math.PI * 2);
+            ctx.fill();
+            this.moons.forEach(moon => {
+                ctx.fillStyle = 'grey';
+                ctx.beginPath();
+                ctx.arc(moon.x, moon.y, moon.size / 2, 0, Math.PI * 2);
+                ctx.fill();
+            });
+        }
+    };
+}
+
+function createPlanet_type2() {
+    return {
+        name: 'planet_type2',
+        color: 'green',
+        size: 50,
+        speed: 1,
+        x: 0,
+        y: 0,
+        moons: [
+            { size: 5, orbitRadius: 20, angle: 0, orbitSpeed: 0.03 },
+            { size: 10, orbitRadius: 30, angle: Math.PI / 4, orbitSpeed: -0.01 }
+        ],
+        updatePosition: function() {
+            this.x -= this.speed;
+            this.moons.forEach(moon => {
+                moon.angle += moon.orbitSpeed;
+                moon.x = this.x + Math.cos(moon.angle) * moon.orbitRadius;
+                moon.y = this.y + Math.sin(moon.angle) * moon.orbitRadius;
+            });
+        },
+        draw: function(ctx) {
+            ctx.fillStyle = this.color;
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.size / 2, 0, Math.PI * 2);
+            ctx.fill();
+            this.moons.forEach(moon => {
+                ctx.fillStyle = 'grey';
+                ctx.beginPath();
+                ctx.arc(moon.x, moon.y, moon.size / 2, 0, Math.PI * 2);
+                ctx.fill();
+            });
+        }
+    };
+}
+
+function createPlanet_type3() {
+    return {
+        name: 'planet_type3',
+        color: 'purple',
+        size: 60,
+        speed: 1,
+        x: 0,
+        y: 0,
+        moons: [
+            { size: 5, orbitRadius: 20, angle: 0, orbitSpeed: 0.02 },
+            { size: 10, orbitRadius: 30, angle: Math.PI / 4, orbitSpeed: -0.01 },
+            { size: 15, orbitRadius: 50, angle: Math.PI / 2, orbitSpeed: 0.03 }
+        ],
+        updatePosition: function() {
+            this.x -= this.speed;
+            this.moons.forEach(moon => {
+                moon.angle += moon.orbitSpeed;
+                moon.x = this.x + Math.cos(moon.angle) * moon.orbitRadius;
+                moon.y = this.y + Math.sin(moon.angle) * moon.orbitRadius;
+            });
+        },
+        draw: function(ctx) {
+            ctx.fillStyle = this.color;
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.size / 2, 0, Math.PI * 2);
+            ctx.fill();
+            this.moons.forEach(moon => {
+                ctx.fillStyle = 'grey';
+                ctx.beginPath();
+                ctx.arc(moon.x, moon.y, moon.size / 2, 0, Math.PI * 2);
+                ctx.fill();
+            });
+        }
+    };
+}
+
+// Array of planet creation functions
+const planetCreationFunctions = [
+    createPlanet_type0,
+    createPlanet_type1,
+    createPlanet_type2,
+    createPlanet_type3
 ];
+
 const planets = [];
 const maxPlanets = 10;
 
 // Generate random planets
 function generatePlanets() {
     if (planets.length < maxPlanets) {
-        const planetType = planetTypes[Math.floor(Math.random() * planetTypes.length)];
-        const size = planetType.size;
-        const x = canvasWidth + size;
-        const y = Math.random() * (canvasHeight - size);
-        planets.push({
-            ...planetType,
-            x: x,
-            y: y,
-            speed: 1
-        });
+        const createPlanet = planetCreationFunctions[Math.floor(Math.random() * planetCreationFunctions.length)];
+        const planet = createPlanet();
+        planet.x = canvasWidth + planet.size;
+        planet.y = Math.random() * (canvasHeight - planet.size);
+        planets.push(planet);
     }
 }
 
 // Update planet positions
 function updatePlanets() {
     planets.forEach((planet, index) => {
-        planet.x -= planet.speed;
+        planet.updatePosition();
         if (planet.x < -planet.size) {
             planets.splice(index, 1); // Remove off-screen planets
         }
@@ -57,22 +184,7 @@ function updatePlanets() {
 
 // Draw planet
 function drawPlanet(planet) {
-    ctx.fillStyle = planet.color;
-    ctx.beginPath();
-    ctx.arc(planet.x, planet.y, planet.size / 2, 0, Math.PI * 2);
-    ctx.fill();
-    
-    if (planet.moons) {
-        planet.moons.forEach(moon => {
-            moon.angle = (moon.angle || 0) + moon.orbitSpeed;
-            const moonX = planet.x + Math.cos(moon.angle) * moon.orbitRadius;
-            const moonY = planet.y + Math.sin(moon.angle) * moon.orbitRadius;
-            ctx.fillStyle = 'grey';
-            ctx.beginPath();
-            ctx.arc(moonX, moonY, moon.size / 2, 0, Math.PI * 2);
-            ctx.fill();
-        });
-    }
+    planet.draw(ctx);
 }
 
 // Draw ship
