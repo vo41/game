692d205386e67e37923764773d77acf90878b1d7
vo41 <161795582+vo41@users.noreply.github.com>
2024-08-22 16:34:59 +0100

    Update planet_type3.js

--- planets/planet_type3.js
+++ planets/planet_type3.js
@@ -1,54 +1,33 @@
-export function createPlanet_type3() {
+function createPlanet_type3() {
     return {
         name: 'planet_type3',
-        color: 'red',
-        size: 30,
+        color: 'purple',
+        size: 50,
         speed: 1,
         x: 0,
         y: 0,
         moons: [
-            {
-                size: 10,
-                distance: 40,
-                angle: 0,
-                speed: 0.05,
-            },
-            {
-                size: 15,
-                distance: 60,
-                angle: Math.PI / 2,
-                speed: 0.03,
-            },
-            {
-                size: 20,
-                distance: 80,
-                angle: Math.PI,
-                speed: 0.02,
-            }
+            { x: 0, y: 0, size: 20, orbitRadius: 60, angle: 0, orbitSpeed: 0.01 },
+            { x: 0, y: 0, size: 20, orbitRadius: 90, angle: Math.PI / 4, orbitSpeed: 0.015 },
+            { x: 0, y: 0, size: 20, orbitRadius: 120, angle: Math.PI / 2, orbitSpeed: 0.02 }
         ],
-        
         updatePosition: function() {
             this.x -= this.speed;
             this.moons.forEach(moon => {
-                moon.angle += moon.speed;
+                moon.angle += moon.orbitSpeed;
+                moon.x = this.x + Math.cos(moon.angle) * moon.orbitRadius;
+                moon.y = this.y + Math.sin(moon.angle) * moon.orbitRadius;
             });
         },
-        
         draw: function(ctx) {
-            // Draw planet
             ctx.fillStyle = this.color;
             ctx.beginPath();
             ctx.arc(this.x, this.y, this.size / 2, 0, Math.PI * 2);
             ctx.fill();
-
-            // Draw moons
             this.moons.forEach(moon => {
-                const moonX = this.x + Math.cos(moon.angle) * moon.distance;
-                const moonY = this.y + Math.sin(moon.angle) * moon.distance;
-
-                ctx.fillStyle = 'blue';
+                ctx.fillStyle = 'lightpurple';
                 ctx.beginPath();
-                ctx.arc(moonX, moonY, moon.size / 2, 0, Math.PI * 2);
+                ctx.arc(moon.x, moon.y, moon.size / 2, 0, Math.PI * 2);
                 ctx.fill();
             });
         }
