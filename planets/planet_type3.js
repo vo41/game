45e031ae234cbab9256770d4ef73d324be40ac4a
vo41 <161795582+vo41@users.noreply.github.com>
2024-08-22 17:00:41 +0100

    Update planet_type3.js

--- planets/planet_type3.js
+++ planets/planet_type3.js
@@ -7,9 +7,9 @@ function createPlanet_type3() {
         x: 0,
         y: 0,
         moons: [
-            { x: 0, y: 0, size: 5, orbitRadius: 40, angle: 0, orbitSpeed: 0.01 },
-            { x: 0, y: 0, size: 10, orbitRadius: 55, angle: Math.PI / 4, orbitSpeed: 0.02 },
-            { x: 0, y: 0, size: 15, orbitRadius: 70, angle: Math.PI / 2, orbitSpeed: 0.03 }
+            { x: 0, y: 0, size: 5, orbitRadius: 50, angle: 0, orbitSpeed: 0.01 },
+            { x: 0, y: 0, size: 10, orbitRadius: 60, angle: Math.PI / 4, orbitSpeed: 0.02 },
+            { x: 0, y: 0, size: 10, orbitRadius: 70, angle: Math.PI / 2, orbitSpeed: -0.03 }
         ],
         updatePosition: function() {
             // Update moon positions
