701fe78ead4c2ec0dc1914be987329e453733e7b
vo41 <161795582+vo41@users.noreply.github.com>
2024-08-22 16:33:01 +0100

    Update planet_type1.js

--- planets/planet_type1.js
+++ planets/planet_type1.js
@@ -1,39 +1,33 @@
-export function createPlanet_type1() {
+function createPlanet_type1() {
     return {
         name: 'planet_type1',
-        color: 'red',
+        color: 'blue',
         size: 30,
         speed: 1,
         x: 0,
         y: 0,
-        moon: {
-            size: 10,
-            distance: 40,
-            angle: 0,
-            speed: 0.05,
-        },
-        
+        moons: [
+            { x: 0, y: 0, size: 10, orbitRadius: 40, angle: 0, orbitSpeed: 0.01 }
+        ],
         updatePosition: function() {
             this.x -= this.speed;
-            this.moon.angle += this.moon.speed;
+            this.moons.forEach(moon => {
+                moon.angle += moon.orbitSpeed;
+                moon.x = this.x + Math.cos(moon.angle) * moon.orbitRadius;
+                moon.y = this.y + Math.sin(moon.angle) * moon.orbitRadius;
+            });
         },
-        
         draw: function(ctx) {
-            // Draw planet
-                        ctx.fillStyle = this.color;
+            ctx.fillStyle = this.color;
             ctx.beginPath();
             ctx.arc(this.x, this.y, this.size / 2, 0, Math.PI * 2);
             ctx.fill();
-
-            // Calculate moon position
-            const moonX = this.x + Math.cos(this.moon.angle) * this.moon.distance;
-            const moonY = this.y + Math.sin(this.moon.angle) * this.moon.distance;
-
-            // Draw moon
-            ctx.fillStyle = 'blue';
-            ctx.beginPath();
-            ctx.arc(moonX, moonY, this.moon.size / 2, 0, Math.PI * 2);
-            ctx.fill();
+            this.moons.forEach(moon => {
+                ctx.fillStyle = 'lightblue';
+                ctx.beginPath();
+                ctx.arc(moon.x, moon.y, moon.size / 2, 0, Math.PI * 2);
+                ctx.fill();
+            });
         }
     };
 }
